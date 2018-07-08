f9a23d5ec6524f5483306f29d1a02a837355c580
rowlindsay <rowlindsay@gmail.com>
2018-07-08 14:57:34 -0700

    initial commit for backend api

--- app/data.js
+++ app/data.js
@@ -18,6 +18,16 @@ var app = new Vue({
   methods: {
     signIn: function() {
         console.log('signing in user ' + this.userEntry.username);
+
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() {
+          if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+            console.log(xmlHttp.responseText);
+        }
+        xmlHttp.open("GET", "http://localhost:3000/account?username=rowan", true);
+        xmlHttp.send(null);
+
         this.signedIn = true;
         this.user.username = this.userEntry.username;
         this.userEntry.clear();
---
+++ backend/routes/routes.js
@@ -0,0 +1,27 @@
+var appRouter = function(app) {
+
+  app.get("/", function(req, res) {
+    res.send("Hello World");
+  });
+
+  app.get("/account", function(req, res) {
+
+    console.log(req.query.username);
+
+    var accountMock = {
+        "username": "rowan",
+        "password": "pWord",
+        "twitter": "@rowlindsay"
+    }
+    if(!req.query.username) {
+        return res.send({"status": "error", "message": "missing username"});
+    } else if(req.query.username != accountMock.username) {
+        return res.send({"status": "error", "message": "wrong username"});
+    } else {
+        return res.send(accountMock);
+    }
+});
+
+}
+
+module.exports = appRouter;
