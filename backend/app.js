dbcf7ba25ac4fab4edf7e81349a47549abb8507d
rowlindsay <rowlindsay@gmail.com>
2018-08-05 20:22:19 +0000

    passport auth iimplementation with successful redirects and local db

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,39 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
+var dbConfig = require('./db.js');
+var mongoose = require('mongoose');
+mongoose.connect(dbConfig.url);
+
+// Configuring Passport
+var passport = require('passport');
+
+var User = require('./model/user');
+
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+var expressSession = require('express-session');
+app.use(expressSession({
+  secret: 'mySecretKey'
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
 const cors = require('cors');
-
 app.use(cors());
 
 var routes = require("./routes/routes.js")(app);
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,168 +1,88 @@
-const https = require('https');
-const apiURL = 'http://vivalasalsa.ca/api/';
-const redirect = apiURL + 'rauth/update';
-const clientId = "eaXyayShEsXmug";
-const secret = require('./client-secret');
-const querystring = require('querystring');
-const btoa = require('btoa');
-const atob = require('atob');
+var https = require('https'),
+  passport = require('passport'),
+  crypto = require('crypto'),
+  mongoose = require('mongoose'),
+  RedditStrategy = require('passport-reddit').Strategy,
+  client = require('../client');
 
-// TODO: rework to use local database and authentication
-// TODO: test
+//TODO: update front end / provide better views for welcome&login
+  //TODO: figure out how to integrate vue with redirect
 
-var AuthRequest = function(user, key) {
-  this.user = user;
-  this.key = key;
-  this.authorized = false;
-  this.token = null;
-  this.state_id = null;
-};
+var User = require('../model/user');
 
-AuthRequest.prototype.stateGen = function() {
-  this.state_id = btoa(`{ "user": "${this.user}","key": "${this.key}" }`);
-  return this.state_id;
-};
-
-function basicAuthEncode(user, pass) {
-  return 'Basic ' + Buffer.from(user + ':' + pass).toString('base64');
-}
-
-function decodeState(state) {
-  var decode = JSON.parse(atob(state));
-  return {
-    user: decode.user,
-    key: decode.key
+passport.use(new RedditStrategy({
+  clientID: client.clientID,
+  clientSecret: client.clientSecret,
+  callbackURL: "http://vivalasalsa.ca/api/auth/reddit/callback"
+},
+  function(accessToken, refreshToken, profile, done) {
+    console.log('authenticating user...');
+    User.findOne({
+      'redditId': profile.id
+    }, function(err, result) {
+      console.log('function');
+      if (!result) {
+        console.log('no record found for user, creating...');
+        user = new User({
+          redditId: profile.id,
+        })
+        user.save(function(err) {
+          console.log('saving new user...');
+          if (err) console.log(err);
+        });
+        return done(err, user);
+      } else {
+        console.log('found user');
+        return done(err, result);
+      }
+    });
+    console.log('auth done.');
   }
-}
-
-var reqs = {}
-
-function findRequest(state) {
-  var request = reqs[state];
-  if (request == null)
-    return {
-      error: 'does not exist'
-    };
-  else if (decodeState(state).key != request.key)
-    return {
-      error: 'key mismatch'
-    };
-  else
-    return request;
-}
-
-function AuthURL(state) {
-  this.clientId = clientId;
-  this.response_type = "code";
-  this.state = state;
-  this.redirect = redirect;
-  this.duration = "permanent";
-  this.scope = "read,subscribe";
-}
-
-AuthURL.prototype.toString = function() {
-  return `https://www.reddit.com/api/v1/authorize?`
-    + `client_id=${this.clientId}&response_type=${this.response_type}`
-    + `&state=${this.state}&redirect_uri=${this.redirect}`
-    + `&duration=${this.duration}&scope=${this.scope}`;
-}
+));
 
 var appRouter = function(app) {
 
-  app.get("/", function(req, res) {
+  app.get('/', function(req, res) {
     res.send("This is the backend node for reddit-resub");
   });
 
-  app.get("/rauth/make", function(req, res) {
-    var user = req.query.user;
-    var key = req.query.key;
-    var request = new AuthRequest(user, key);
-    var state = request.stateGen()
-    reqs[state] = request;
-    var url = new AuthURL(state);
-    res.send(url.toString());
+  app.get('/home', ensureAuthenticated, function(req, res) {
+    res.send("This is the home page, congrats on logging in!");
   });
 
-  app.get("/rauth/status", function(req, res) {
-    // TODO: update front-end call
-    var state = req.query.state;
-    var request = findRequest(state);
-    if (request.error)
-      res.send(request.error);
-    else {
-      res.send({
-        authorized: request.status
-      });
-    }
+  app.get('/welcome', function(req, res) {
+    res.send("Welcome! Log in with Reddit to begin");
   });
 
-  app.get("/rauth/update", function(req, res) {
-    // TODO: display a better landing page for redirect
-    if (req.query.error) {
-      res.send("there was an error authorizing reddit");
+  app.get('/auth/reddit', function(req, res, next) {
+    req.session.state = crypto.randomBytes(32).toString('hex');
+    passport.authenticate('reddit', {
+      state: req.session.state,
+      duration: 'permanent',
+    })(req, res, next);
+  });
+
+  app.get('/auth/reddit/callback', function(req, res, next) {
+    console.log('recieved auth callback')
+    // Check for origin via state token
+    if (req.query.state == req.session.state) {
+      console.log('state confirmed');
+      passport.authenticate('reddit', {
+        successRedirect: '/api/home',
+        failureRedirect: '/api/welcome'
+      })(req, res, next);
     } else {
-      var state = req.query.state;
-      var request = findRequest(state);
-      if (request.error)
-        res.send("Could not find authorization request matching the provided user and key");
-      var code = req.query.code;
-      postTokenRetrieve(code).then(function(token) {
-        request.token = token;
-        request.authorized = true;
-      });
-      res.send(`User authorized; Refresh status to continue.`);
+      next(new Error(403));
     }
-  });
-
-  app.get("/rauth/retrieve", function(req, res) {
-    // TODO: update front-end call
-    var request = findRequest(req.query.state);
-    if (request.authorized) {
-      var token = request.token;
-      reqs.pop(request);
-      res.send(token);
-    }
-    else res.send("token could not be found");
   });
 
 }
 
-async function postTokenRetrieve(code) {
-  return new Promise(resolve => {
-    var data = querystring.stringify({
-      "grant_type": "authorization_code",
-      "code": code,
-      "redirect_uri": redirect
-    });
-
-    var options = {
-      hostname: 'ssl.reddit.com',
-      port: 443,
-      path: '/api/v1/access_token',
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': data.length,
-        'Authorization': basicAuthEncode(clientId, secret)
-      }
-    }
-
-    var req = https.request(options, (res) => {
-      var response = "";
-      res.on("data", function(data) {
-        response += data;
-      });
-      res.on("end", function() {
-        resolve(JSON.parse(response).access_token);
-      });
-    });
-
-    req.on('error', (e) => {
-      console.error(e);
-    });
-
-    req.write(data); req.end();
-  });
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('welcome');
 }
 
 module.exports = appRouter;
