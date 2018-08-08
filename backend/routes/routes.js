8a432dcbce85ddef7dd95034d4d26c9d0f16bde3
rowlindsay <rowlindsay@gmail.com>
2018-08-08 07:27:02 +0000

    endpoint views and improved routing

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -5,10 +5,95 @@ var https = require('https'),
   RedditStrategy = require('passport-reddit').Strategy,
   client = require('../client');
 
-//TODO: figure out how to handle callback without serving pages
+//TODO: implement basic auth and make reddit register only
+
+var appRouter = function(app) {
+
+  app.get('/', function(req, res) {
+    res.send("This is the backend node for reddit-resub");
+  });
+
+  app.get('/session', ensureAuthenticated, function(req, res) {
+    var user = req.user;
+    var uid = user.id;
+    res.send({
+      authorized: true,
+      user: uid
+    });
+  });
+
+  // Static page servers for navigating users through authorization
+  app.get('/signup/new', function(req, res) {
+    res.sendfile('form.html', {
+      root: __dirname + '/../public'
+    });
+  });
+
+  app.get('/signup/confirm', function(req, res) {
+    res.sendfile('done.html', {
+      root: __dirname + '/../public'
+    });
+  });
+
+  app.get('/signup/error', function(req, res) {
+    res.sendfile('autherr.html', {
+      root: __dirname + '/../public'
+    });
+  })
+
+  // Reddit Token Retrieval Flow:
+  // Send request
+  app.get('/auth/reddit', function(req, res, next) {
+    req.session.state = crypto.randomBytes(32).toString('hex');
+    passport.authenticate('reddit', {
+      state: req.session.state,
+      duration: 'permanent',
+    })(req, res, next);
+  });
+
+  // Handle Callback
+  app.get('/auth/reddit/callback', function(req, res, next) {
+    console.log('recieved auth callback')
+    // Check for origin via state token
+    if (req.query.state == req.session.state) {
+      console.log('state confirmed');
+      passport.authenticate('reddit', {
+        // TODO: implement middle point for doing things?
+        successRedirect: '/api/signup/confirm',
+        // TODO: update fail dest
+        failureRedirect: '/api/signup/error'
+      })(req, res, next);
+    } else {
+      next(new Error(403));
+    }
+  });
+  // end
+
+}
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  // Redirect user to registration form
+  res.redirect('signup/new');
+}
+
+// Database
 
 var User = require('../model/user');
 
+function saveNewUser(id) {
+  user = new User({
+    'id': id,
+  })
+  user.save(function(err) {
+    console.log('saving new user...');
+    if (err) console.log(err);
+  });
+}
+
+// Passport Strategy
 passport.use(new RedditStrategy({
   clientID: client.clientID,
   clientSecret: client.clientSecret,
@@ -31,67 +116,5 @@ passport.use(new RedditStrategy({
     console.log('auth done.');
   }
 ));
-
-var appRouter = function(app) {
-
-  app.get('/', function(req, res) {
-    res.send("This is the backend node for reddit-resub");
-  });
-
-  app.get('/session', ensureAuthenticated, function(req, res) {
-    var user = req.user;
-    var uid = user.id;
-    res.send({
-      authorized: true,
-      user: uid
-    });
-  });
-
-  app.get('/failauth', function(req, res) {
-    res.send({
-      authorized: false
-    });
-  });
-
-  app.get('/auth/reddit', function(req, res, next) {
-    req.session.state = crypto.randomBytes(32).toString('hex');
-    passport.authenticate('reddit', {
-      state: req.session.state,
-      duration: 'permanent',
-    })(req, res, next);
-  });
-
-  app.get('/auth/reddit/callback', function(req, res, next) {
-    console.log('recieved auth callback')
-    // Check for origin via state token
-    if (req.query.state == req.session.state) {
-      console.log('state confirmed');
-      passport.authenticate('reddit', {
-        successRedirect: '/api/session',
-        failureRedirect: '/api/failauth'
-      })(req, res, next);
-    } else {
-      next(new Error(403));
-    }
-  });
-
-}
-
-function saveNewUser(id) {
-  user = new User({
-    'id': id,
-  })
-  user.save(function(err) {
-    console.log('saving new user...');
-    if (err) console.log(err);
-  });
-}
-
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('auth/reddit');
-}
 
 module.exports = appRouter;
